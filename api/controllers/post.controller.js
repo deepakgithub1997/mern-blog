a1444a17b39c421408eafda184bb195259fec465
deepak <deepak.v@somaiya.edu>
2024-03-19 12:45:07 +0530

    add invalid title code

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -3,13 +3,22 @@ import { errorHandler } from '../utils/error.js';
 import { createSlug } from '../utils/create-slug.js';
 
 export const create = async (req, res, next) => {
+  const { title, content } = req.body;
+
   if (!req.user.isAdmin) {
     return next(errorHandler(403, 'You are not allowed to create a post'));
   }
   if (!req.body.title || !req.body.content) {
     return next(errorHandler(400, 'Please provide all required fileds'));
   }
-
+  try {
+    const invalidTitle = Post.findOne({ title });
+    if (invalidTitle) {
+      return next(errorHandler(400, 'Title should be unique'));
+    }
+  } catch (error) {
+    console.log(error);
+  }
   const slug = createSlug(req.body.title);
   console.log(slug);
   const newPost = new Post({
