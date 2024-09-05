9f19ce647d0e468ecdcad371e45711fedd9f1b2f
deepak <deepak.v@somaiya.edu>
2024-09-05 12:20:10 +0530

    complete project and about us page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import CallToAction from '../components/CallAction';
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="min-h-screen max-w-2xl mx-auto flex flex-col items-center justify-center gap-5">
+      <h1 className='text-3xl font font-semibold text-center my-7 '>Projects</h1>
+      <p className='text-center text-md text-gray-500'>Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+      <CallToAction />
+
+    </div>
   )
 }
 
