88c697f8f09ca61ba09f066348f3313745828bd0
deepak <deepak.v@somaiya.edu>
2024-09-24 10:42:18 +0530

    project changes for live

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,30 +1,12 @@
-import React from 'react'
+import React from 'react';
+
 const about = () => {
   return (
-    <div className='min-h-screen flex items-center justify-center'>
-      <div className='max-w-2xl mx-auto p-3 text-center'>
-        <div className=''>
-          <h1 className="text-3xl font font-semibold text-center my-7">About Deepak Blog's</h1>
-          <div className='text-md text-gray-500 flex flex-col gap-6'>
-            <p>
-              Welcome to Deepak's Blog! This blog was created by Deepak as a personal project to
-              share his thoughts and ideas with the world. Deepak is a passionate developer who loves to
-              write about technology, coding, and everything in between.
-            </p>
-            <p>
-              On this blog, you'll find weekly articles and tutorials on topics such as web development,
-              software engineering, and programming languages. Deepak is always learning and exploring
-              new technologies, so be sure to check back often for new content!
-            </p>
-            <p>
-              We encourage you to leave comments on our posts and engage with other readers. You can like
-              other people's comments and reply to them as well. We believe that a community of learners
-              can help each other grow and improve.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div >
+    <>
+      <section class="">
+        <h2 class="">About</h2>
+      </section>
+    </>
   )
 }
 
